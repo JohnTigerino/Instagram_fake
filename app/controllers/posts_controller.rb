7e0b1270a71d4cd57533804d04f0e84204d4a724
John Tigerino <johntigerino2103@gmail.com>
2024-11-19 10:06:13 -0600

    Proyecto 1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,77 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id # Asignar el usuario actual como el dueño del post
+
+    if @post.save
+      flash[:success] = "Success!"
+      redirect_to post_path(@post)
+    else
+      flash.now[:error] = @post.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+
+    # Validar que solo el dueño del post pueda editarlo
+    unless @post.user_id == current_user.id
+      flash[:error] = "You are not authorized to edit this post."
+      redirect_to posts_path
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    # Validar que solo el dueño del post pueda actualizarlo
+    if @post.user_id == current_user.id
+      if @post.update(post_params)
+        flash[:success] = "El post ffue actualizado correctamente!"
+        redirect_to post_path(@post)
+      else
+        flash.now[:error] = @post.errors.full_messages.to_sentence
+        render :edit
+      end
+    else
+      flash[:error] = "No estas autorizado para actualizar el post."
+      redirect_to posts_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+  
+    # Verifica si el usuario actual es el dueño del post
+    if @post.user_id == current_user.id
+      @post.destroy
+      flash[:success] = "El post ha sido eliminado correctamente."
+      redirect_to posts_path
+    else
+      flash[:error] = "No puedes eliminar este post."
+      redirect_to posts_path
+    end
+  end
+  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description, images: [])
+  end
+end
